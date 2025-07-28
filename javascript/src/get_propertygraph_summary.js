2ebc3539fe535243710647736acdb11a1cbe5570
briokeef <briokeef@amazon.com>
2025-07-28 14:00:53 -0400

    Added best practices for javascript example

--- javascript/src/get_propertygraph_summary.js
+++ javascript/src/get_propertygraph_summary.js
@@ -1,13 +1,17 @@
 import { NeptunedataClient, GetPropertygraphSummaryCommand } from "@aws-sdk/client-neptunedata";
 import {inspect} from "util";
+import {NodeHttpHandler} from "@smithy/node-http-handler";
 
 class NeptuneDataClient {
     constructor() {
         const clientConfig = {
-            endpoint: 'https://my-cluster-name.cluster-abcdefgh1234.my-region.neptune.amazonaws.com:my-port',
+            endpoint: 'https://my-cluster-name.cluster-abcdefgh1234.my-region.neptune.amazonaws.com:my-port', // Replace with your endpoint
             sslEnabled: true,
-            region: 'my-region',
-            maxAttempts: 1
+            region: 'my-region', // replace with your region
+            maxAttempts: 1,  // do not retry
+            requestHandler: new NodeHttpHandler({
+                requestTimeout: 0  // no client timeout
+            })
         };
 
         this._client = new NeptunedataClient(clientConfig);
